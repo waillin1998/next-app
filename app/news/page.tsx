aa9071a660fe5b0a97950d1aab7794033623a612
Kafka <w24006@osfl.ac.jp>
2025-05-30 12:23:35 +0900

    ニュースにSSRを適用

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,8 +1,10 @@
 import { getNewsList } from "@/app/_libs/microcms";
 import NewsList from "@/app/components/NewsList";
 import Pagination from "@/app/components/Pagination";
-import SearchField from "@/app/components/SearchField";
 import { NEWS_LIST_LIMIT } from "@/app/_constants";
+import SearchField from "@/app/components/SearchField";
+
+export const revalidate = 0;
 
 export default async function Page() {
   const { contents: news, totalCount } = await getNewsList({
